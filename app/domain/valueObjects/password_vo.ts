c5ea32122511761320f9448d003180904b3df08b
kdumerchat <kyrill.dumerchat@kwh50.io>
2025-05-20 11:50:00 +0200

    create VO to determine minimum password complexity

---
+++ app/domain/valueObjects/password_vo.ts
@@ -0,0 +1,21 @@
+export class Password {
+  constructor(private password: string) {
+    this.validate();
+  }
+
+  validate() {
+    if (this.password.length < 6) {
+      throw new Error('Password must be at least 6 characters long');
+    }
+    if (this.password.length > 50) {
+      throw new Error('Password must be at most 50 characters long');
+    }
+    if (!this.password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,50}$/)) {
+      throw new Error('Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character');
+    }
+  }
+
+  getValue() {
+    return this.password;
+  }
+}
