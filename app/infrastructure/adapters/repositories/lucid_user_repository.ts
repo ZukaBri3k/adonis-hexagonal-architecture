472514ae0c02f37b41c2e326a025014a32b0fb51
kdumerchat <kyrill.dumerchat@kwh50.io>
2025-05-20 09:54:46 +0200

    impl all abstract CRUD methods

--- app/infrastructure/adapters/repositories/lucid_user_repository.ts
+++ app/infrastructure/adapters/repositories/lucid_user_repository.ts
@@ -1,5 +1,6 @@
+import { CreateUserDTO, UpdateUserDTO } from "#domain/dto/user_dto";
 import { User } from "#domain/entities/user_entity";
-import { UserRepository } from "#domain/repositories/user_repository";
+import { UserRepository } from "#domain/ports/user_repository";
 import LucidModelUser from "#models/lucid_model_user";
 
 
@@ -7,16 +8,49 @@ export class LucidUserRepository implements UserRepository {
   toDomainModel(user: LucidModelUser): User {
     return new User({
       id: user.id,
-      username: user.fullName,
+      username: user.username,
       password: user.password,
+      email: user.email,
       createdAt: user.createdAt.toJSDate(),
       updatedAt: user.updatedAt?.toJSDate() || null,
     });
   }
 
-  async findById(id: number): Promise<User | null> {
-    const user = await LucidModelUser.find(id);
+  async findById(id: number): Promise<User> {
+    const user = await LucidModelUser.findOrFail(id);
 
-    return user ? this.toDomainModel(user) : null;
+    return this.toDomainModel(user);
+  }
+
+  async findAll(): Promise<User[]> {
+    const users = await LucidModelUser.all();
+
+    return users.map((user) => this.toDomainModel(user));
+  }
+
+  async create(userProps: CreateUserDTO): Promise<User> {
+    const user = await LucidModelUser.create(userProps);
+
+    return this.toDomainModel(user);
+  }
+
+  async update(id: number, userProps: UpdateUserDTO): Promise<User> {
+    const user = await LucidModelUser.findOrFail(id);
+
+    user.username = userProps.username ?? user.username;
+    user.password = userProps.password ?? user.password;
+    user.email = userProps.email ?? user.email;
+
+    await user.save();
+
+    return this.toDomainModel(user);
+  }
+
+  async delete(id: number): Promise<User> {
+    const user = await LucidModelUser.findOrFail(id);
+
+    await user.delete();
+
+    return this.toDomainModel(user);
   }
 }
