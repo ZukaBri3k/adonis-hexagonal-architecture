e5531a9123dc143071d2f2cda2cc4a509aab8cda
kdumerchat <kyrill.dumerchat@kwh50.io>
2025-05-20 11:16:14 +0200

    adding cutoms domain errors

--- app/domain/services/user_service.ts
+++ app/domain/services/user_service.ts
@@ -1,8 +1,9 @@
 import { inject } from "@adonisjs/core";
 import { userContract } from "#infrastructure/adapters/contracts/user_contract";
 import { UserRepository } from "#domain/ports/user_repository";
-import { UpdateUserDTO } from "#domain/dto/user_dto";
+import { CreateUserDTO, UpdateUserDTO } from "#domain/dto/user_dto";
 import { UserMapper } from "#domain/mappers/user_mapper";
+import { UserAlreadyExistsException, UserNotFoundException } from "#domain/exceptions/user_exceptions";
 
 
 
@@ -11,30 +12,42 @@ export class UserService {
   constructor(private userRepository: UserRepository) {}
 
   async getUserById(id: number): Promise<userContract> {
-    const user = await this.userRepository.findById(id);
-
-    return UserMapper.toContract(user);
+    try {
+      const user = await this.userRepository.findById(id);
+      return UserMapper.toContract(user);
+    } catch (error) {
+      throw new UserNotFoundException(id);
+    }
   }
 
   async getAllUsers(): Promise<userContract[]> {
     return (await this.userRepository.findAll()).map((user) => UserMapper.toContract(user));
   }
 
-  async createUser(username: string, password: string, email: string): Promise<userContract> {
-    const user = await this.userRepository.create({ username, password, email });
-
-    return UserMapper.toContract(user);
+  async createUser(props: CreateUserDTO): Promise<userContract> {
+    try {
+      const user = await this.userRepository.create(props);
+      return UserMapper.toContract(user);
+    } catch (error) {
+      throw new UserAlreadyExistsException(props.username, props.email);
+    }
   }
 
   async updateUser(id: number, userProps: UpdateUserDTO): Promise<userContract> {
-    const user = await this.userRepository.update(id, userProps);
-
-    return UserMapper.toContract(user);
+    try {
+      const user = await this.userRepository.update(id, userProps);
+      return UserMapper.toContract(user);
+    } catch (error) {
+      throw new UserNotFoundException(id);
+    }
   }
 
   async deleteUser(id: number): Promise<userContract> {
-    const user = await this.userRepository.delete(id);
-
-    return UserMapper.toContract(user);
+    try {
+      const user = await this.userRepository.delete(id);
+      return UserMapper.toContract(user);
+    } catch (error) {
+      throw new UserNotFoundException(id);
+    }
   }
 }
