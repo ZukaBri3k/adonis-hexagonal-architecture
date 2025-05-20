a08d4daf810f5cec614af7b31814721e0968cc3b
kdumerchat <kyrill.dumerchat@kwh50.io>
2025-05-20 09:54:09 +0200

    add all crud methods

--- app/domain/services/user_service.ts
+++ app/domain/services/user_service.ts
@@ -1,6 +1,7 @@
 import { inject } from "@adonisjs/core";
-import { userContract } from "#infrastructure/contracts/user_contract";
-import { UserRepository } from "#domain/repositories/user_repository";
+import { userContract } from "#infrastructure/adapters/contracts/user_contract";
+import { UserRepository } from "#domain/ports/user_repository";
+import { UpdateUserDTO } from "#domain/dto/user_dto";
 
 
 
@@ -8,15 +9,66 @@ import { UserRepository } from "#domain/repositories/user_repository";
 export class UserService {
   constructor(private userRepository: UserRepository) {}
 
-  async getUserById(id: number): Promise<userContract | null> {
-    const user = await this.userRepository.findById(id)
+  async getUserById(id: number): Promise<userContract> {
+    const user = await this.userRepository.findById(id);
 
-    return user ? new userContract({
+    return new userContract({
       id: user.id,
       username: user.username,
+      email: user.email,
       password: user.password,
       createdAt: user.createdAt,
       updatedAt: user.updatedAt,
-    }) : null;
+    });
+  }
+
+  async getAllUsers(): Promise<userContract[]> {
+    return (await this.userRepository.findAll()).map((user) => new userContract({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      password: user.password,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    }));
+  }
+
+  async createUser(username: string, password: string, email: string): Promise<userContract> {
+    const user = await this.userRepository.create({ username, password, email });
+
+    return new userContract({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      password: user.password,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    });
+  }
+
+  async updateUser(id: number, userProps: UpdateUserDTO): Promise<userContract> {
+    const user = await this.userRepository.update(id, userProps);
+
+    return new userContract({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      password: user.password,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    });
+  }
+
+  async deleteUser(id: number): Promise<userContract> {
+    const user = await this.userRepository.delete(id);
+
+    return new userContract({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      password: user.password,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    });
   }
 }
