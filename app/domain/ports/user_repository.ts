e9830236c98f5d04e817a3d2f813a288f3ae65c3
kdumerchat <kyrill.dumerchat@kwh50.io>
2025-05-20 09:53:58 +0200

    add all crud methods

--- app/domain/ports/user_repository.ts
+++ app/domain/ports/user_repository.ts
@@ -1,6 +1,11 @@
+import { CreateUserDTO, UpdateUserDTO } from "#domain/dto/user_dto";
 import { User } from "#domain/entities/user_entity";
 
 
 export abstract class UserRepository {
-  abstract findById(id: number): Promise<User | null>;
+  abstract findById(id: number): Promise<User>;
+  abstract findAll(): Promise<User[]>;
+  abstract create(user: CreateUserDTO): Promise<User>;
+  abstract update(id: number, userProps: UpdateUserDTO): Promise<User>;
+  abstract delete(id: number): Promise<User>;
 }
