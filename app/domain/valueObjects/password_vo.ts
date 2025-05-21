c4b6c9c6d4b6ce18a7fbc3e505665cd0bec0f6eb
kdumerchat <kyrill.dumerchat@kwh50.io>
2025-05-21 14:56:48 +0200

    Add domain error

--- app/domain/valueObjects/password_vo.ts
+++ app/domain/valueObjects/password_vo.ts
@@ -1,3 +1,5 @@
+import { PasswordNotStrongEnoughException } from "#domain/exceptions/user_exceptions";
+
 export class Password {
   constructor(private password: string) {
     this.validate();
@@ -5,13 +7,13 @@ export class Password {
 
   validate() {
     if (this.password.length < 6) {
-      throw new Error('Password must be at least 6 characters long');
+      throw new PasswordNotStrongEnoughException('Password must be at least 6 characters long');
     }
     if (this.password.length > 50) {
-      throw new Error('Password must be at most 50 characters long');
+      throw new PasswordNotStrongEnoughException('Password must be at most 50 characters long');
     }
     if (!this.password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,50}$/)) {
-      throw new Error('Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character');
+      throw new PasswordNotStrongEnoughException('Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character');
     }
   }
 
